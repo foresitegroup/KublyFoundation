5e8a856566e2187255e2d180cf624ddd10d32d4b
foresitegroup <mark@foresitegrp.com>
2016-08-30 16:27:34 -0500

    2016-08-30

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    
+    <title>The Charles E. Kubly Foundation<?php if (isset($PageTitle)) echo " | " . $PageTitle; ?></title>
+    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    
+    <meta name="description" content="<?php if (isset($Description)) echo $Description; ?>">
+    <meta name="keywords" content="<?php if (isset($Keywords)) echo $Keywords; ?>">
+    <meta name="author" content="Foresite Group">
+    
+    <meta name="viewport" content="width=device-width">
+    <link href="//fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link rel="stylesheet" href="inc/main.css">
+    
+    <script type="text/javascript" src="inc/jquery-1.12.4.min.js"></script>
+    <script type="text/javascript">
+      $(document).ready(function() {
+        $("a[href^='http'], a[href$='.pdf']").not("[href*='" + window.location.host + "']").attr('target','_blank');
+      });
+    </script>
+  </head>
+  <body>
+    
+    <div class="kf-header">
+      <div class="site-width">
+        <a href="." id="logo"><img src="images/logo.png" alt="The Charles E. Kubly Foundation"></a>
+
+        <input type="checkbox" id="show-menu" role="button">
+        <label for="show-menu" id="menu-toggle"></label>
+        <div class="menu">
+          <ul>
+            <li>
+              <a href="#">Foundation</a>
+              <ul>
+                <li><a href="#">The Foundation</a></li>
+                <li><a href="#">Charlie's Story</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Board &amp; Leadership</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Signs &amp; Facts</a></li>
+            <li><a href="#">Grants</a></li>
+            <li><a href="#">Beyond the Blues</a></li>
+            <li><a href="#">Connect</a></li>
+            <li class="incrisis"><a href="#">+ In Crisis</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="home-banner">
+      <div class="home-banner-slide" style="background-image: url(images/home-banner.jpg);">
+        <div class="site-width">
+          <h1>A PUBLIC CHARITY DEVOTED TO IMPROVING THE LIVES OF THOSE WITH DEPRESSION</h1>
+
+          <a href="#" class="button">DONATE</a>
+          <a href="#" class="learnmore">LEARN MORE</a>
+        </div>
+      </div>
+    </div>
+    
+    <h1>Content (H1)</h1>
+    
+    Bacon ipsum dolor sit amet sausage bacon biltong, salami drumstick hamburger ham hock. Filet mignon ribeye meatball flank tri-tip tongue boudin, doner pig tenderloin. Beef cow turducken pork belly. Corned beef andouille short loin spare ribs. Short ribs frankfurter pig beef ribs. Sausage salami kielbasa cow jowl. Pork ribeye sirloin sausage bacon ham swine turkey biltong tenderloin boudin beef ribs pig hamburger.<br>
+    <br>
+    
+    Pig shankle andouille venison ham frankfurter strip steak ham hock, swine jerky ball tip flank hamburger. Leberkas cow short loin capicola ham hock bresaola. Pig beef ribs salami shankle, ham hock shank flank kielbasa sausage hamburger tenderloin. Salami shankle prosciutto sausage pork chop tri-tip. Short loin shankle tail capicola bresaola chuck drumstick pork belly t-bone shoulder hamburger salami corned beef leberkas meatloaf. Corned beef t-bone drumstick jowl shoulder brisket sirloin meatball turkey.<br>
+    <br>
+    
+    Bacon sirloin jowl tail pork loin corned beef sausage ribeye rump. Pork chop spare ribs turkey andouille strip steak. Venison pig bresaola ground round. Leberkas frankfurter pastrami prosciutto bresaola jowl.
+
+    Copyright &copy; <?php echo date("Y"); ?> All rights reserved
+
+  </body>
+</html>
