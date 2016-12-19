b04be57976446cefaef3d5959b1ea2f0b7802016
foresitegroup <mark@foresitegrp.com>
2016-12-19 12:01:44 -0600

    Send newsletter sign-ups to MailChimp

--- form-connect.php
+++ form-connect.php
@@ -27,7 +27,42 @@ if ($_POST['confirmationCAP'] == "") {
     $Message .= "\n\n" . $_POST[md5('message' . $_POST['ip'] . $salt . $_POST['timestamp'])];
     
     if (isset($_POST['subscribe'])) {
-      $Message .= "\n\nI would like to receive the Charles E. Kubly newsletter and event information.";
+      $data = [
+        'email'  => $_POST[md5('email' . $_POST['ip'] . $salt . $_POST['timestamp'])],
+        'status' => 'subscribed'
+      ];
+      
+      function syncMailchimp($data) {
+        $apiKey = 'a3820f880f16a587390412816e3beda1-us14';
+        $listId = '30867c8d51';
+
+        $memberId = md5(strtolower($data['email']));
+        $dataCenter = substr($apiKey,strpos($apiKey,'-')+1);
+        $url = 'https://' . $dataCenter . '.api.mailchimp.com/3.0/lists/' . $listId . '/members/' . $memberId;
+
+        $json = json_encode([
+          'email_address' => $data['email'],
+          'status'        => $data['status']
+        ]);
+
+        $ch = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_USERPWD, 'user:' . $apiKey);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);                                                                                                                 
+
+        $result = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        return $httpCode;
+      }
+
+      syncMailchimp($data);
     }
 
     $Message = stripslashes($Message);
--- form-newsletter.php
+++ form-newsletter.php
@@ -3,17 +3,42 @@ $salt = "KublyNewsletterForm";
 
 if ($_POST['confirmationCAP'] == "") {
   if ($_POST[md5('email' . $_POST['ip'] . $salt . $_POST['timestamp'])] != "") {
-    $Subject = "Kubly Foundation Newsletter Signup";
-    $SendTo = "info@charlesekublyfoundation.org";
-    $Headers = "From: Contact Form <newsletterform@charlesekublyfoundation.org/>\r\n";
-    $Headers .= "Reply-To: " . $_POST[md5('email' . $_POST['ip'] . $salt . $_POST['timestamp'])] . "\r\n";
-    $Headers .= "Bcc: mark@foresitegrp.com\r\n";
+    $data = [
+        'email'  => $_POST[md5('email' . $_POST['ip'] . $salt . $_POST['timestamp'])],
+        'status' => 'subscribed'
+      ];
+      
+      function syncMailchimp($data) {
+        $apiKey = 'a3820f880f16a587390412816e3beda1-us14';
+        $listId = '30867c8d51';
 
-    $Message = "I would like to receive the Charles E. Kubly newsletter and event information.\n" . $_POST[md5('email' . $_POST['ip'] . $salt . $_POST['timestamp'])];
+        $memberId = md5(strtolower($data['email']));
+        $dataCenter = substr($apiKey,strpos($apiKey,'-')+1);
+        $url = 'https://' . $dataCenter . '.api.mailchimp.com/3.0/lists/' . $listId . '/members/' . $memberId;
 
-    $Message = stripslashes($Message);
-  
-    mail($SendTo, $Subject, $Message, $Headers);
+        $json = json_encode([
+          'email_address' => $data['email'],
+          'status'        => $data['status']
+        ]);
+
+        $ch = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_USERPWD, 'user:' . $apiKey);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);                                                                                                                 
+
+        $result = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        return $httpCode;
+      }
+
+      syncMailchimp($data);
 
     $nlfeedback = "Thank you for signing up for our newsletter!";
     
