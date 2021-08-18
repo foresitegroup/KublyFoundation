3688d7df03beb19897ea2d11160f55e613ee80e1
marklippert <lippert@gmail.com>
2021-08-18 10:25:05 -0500

    Add new newsletter

--- foundation.php
+++ foundation.php
@@ -249,34 +249,36 @@ include "header.php";
 
 <div id="newsletters">
   <div class="site-width">
-    <a href="pdf/Kubly_Newsletter_Winter_2021.pdf" class="image"><img src="images/newsletter-winter-2021.jpg" alt=""></a>
+    <a href="pdf/Kubly_Newsletter_Summer_2021.pdf" class="image"><img src="images/newsletter-summer-2021.jpg" alt=""></a>
 
     <div class="text">
-      <h3>Winter 2021 Newsletter</h3>
+      <h3>Summer 2021 Newsletter</h3>
 
       <span>What's Inside</span><br>
-      A Message From Molly Kubly Fritz<br>
-      <span>01.</span> Beyond the Blues Recap<br>
+      A Message From New Board President Linda Lundeen<br>
+      <span>01.</span> A Warm Welcome<br>
+      <span>02.</span> Annual Financial Report<br>
       <span>03.</span> Projects Funded<br>
-      <span>04.</span> Endowment Donors<br>
-      <span>05.</span> CEKF Young Leaders Council<br>
+      <span>04.</span> CEKF & Kids Matter<br>
+      <span>05.</span> Thank You Donors<br>
       <br>
 
       <h4>Past Newsletters</h4>
       <div id="past-newsletters">
+        <div>
+          <a href="pdf/Kubly_Newsletter_Summer_2021.pdf">Summer 2021</a><br>
+          <a href="pdf/Kubly_Newsletter_Summer_2020.pdf">Summer 2020</a><br>
+          <a href="pdf/Kubly_Newsletter_Summer_2019.pdf">Summer 2019</a><br>
+          <a href="pdf/Kubly_Newsletter_Summer_2018.pdf">Summer 2018</a><br>
+          <a href="pdf/Kubly_Newsletter_Summer_2017.pdf">Summer 2017</a>
+        </div>
+
         <div>
           <a href="pdf/Kubly_Newsletter_Winter_2021.pdf">Winter 2021</a><br>
           <a href="pdf/Kubly_Newsletter_Winter_2020.pdf">Winter 2020</a><br>
           <a href="pdf/Kubly_Newsletter_Winter_2019.pdf">Winter 2019</a><br>
           <a href="pdf/Kubly_Newsletter_Winter_2018.pdf">Winter 2018</a><br>
           <a href="pdf/Kubly_Newsletter_Winter_2017.pdf">Winter 2017</a>
-        </div>
-
-        <div>
-          <a href="pdf/Kubly_Newsletter_Summer_2020.pdf">Summer 2020</a><br>
-          <a href="pdf/Kubly_Newsletter_Summer_2019.pdf">Summer 2019</a><br>
-          <a href="pdf/Kubly_Newsletter_Summer_2018.pdf">Summer 2018</a><br>
-          <a href="pdf/Kubly_Newsletter_Summer_2017.pdf">Summer 2017</a>
         </div>
       </div> <!-- /#past-newletters -->
     </div> <!-- /.text -->
