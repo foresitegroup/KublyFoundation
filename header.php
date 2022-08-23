be9326e796b17492206718ff217334ca443e5af3
foresitegroup <mark@foresitegrp.com>
2022-08-23 10:28:23 -0500

    Add new analytics tag

--- header.php
+++ header.php
@@ -67,15 +67,24 @@ if (!isset($TopDir)) $TopDir = "";
         if ($('#'+location.href.split("#")[1]).length) AnchorScroll('#'+location.href.split("#")[1]);
       });
     </script>
-
+    
+    <!-- OLD Google Analytics -- THIS WILL STO[ WORKING JULY 1, 2023 -->
     <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
       ga('create', 'UA-37958686-1', 'auto');
       ga('send', 'pageview');
+    </script>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-7X0SMH5HXJ"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-7X0SMH5HXJ');
     </script>
   </head>
   <body>
