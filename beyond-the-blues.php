0a49590542bdeef7d44ef722e0c14345f8e846d9
Mark Lippert <mark@foresitegrp.com>
2023-06-21 10:26:48 -0500

    Update sponsors

--- beyond-the-blues.php
+++ beyond-the-blues.php
@@ -68,50 +68,36 @@ include "header.php";
     <h2>BEYOND THE BLUES 2022 SPONSORS</h2>
 
     <div class="one-third">
-      <h3>Sapphire Level - $20,000</h3>
+      <!-- <h3>Sapphire Level - $20,000</h3>
       <ul>
         <li>Colony Brands</li>
         <li>Meg Fetherston Kasch & Libby Kasch</li>
         <li>Billie Kubly</li>
-      </ul>
+      </ul> -->
 
       <h3>Aqua Level - $10,000</h3>
       <ul>
-        <li>Julie & David Uihlein - in memory of Chucker Aring</li>
+        <li>Michael Schmitz</li>
       </ul>
 
       <h3>Teal Level - $5,000</h3>
       <ul>
-        <li>Hauske Family Foundation</li>
-        <li>William & Sandy Heitz Family Foundation</li>
-        <li>Sandy & Dennis Kuester</li>
-        <li>Cathy & Paul Luber</li>
-        <li>Rite Hite Foundation</li>
-        <li>Rogers Behavioral Health</li>
-        <li>Andrea & Jim Schloemer/Sue & Dan Minahan</li>
-        <li>Michael J. Schmitz</li>
-        <li>Kate & John Wasmer</li>
+        <li>Continental Properties Company, Inc.</li>
+        <li>The Hauske Family Foundation</li>
+        <li>The Sarah & Michael Weiss Family</li>
       </ul>
 
       <h3>Turquoise Level - $2,500</h3>
       <ul>
-        <li>Suzy & John Brennan</li>
-        <li>Children's Wisconsin</li>
+        <li>Lisa & Souheil Badran</li>
+        <li>The Bourne Family</li>
         <li>Crescent Grove Advisors</li>
-        <li>Executive Benefits Network -- Molly & David Fritz</li>
-        <li>Godfrey & Kahn, S.C.</li>
-        <li>Cookie Goris</li>
-        <li>Thomas J. Gould</li>
+        <li>Nancy & John Dickens/Godfrey & Kahn, S.C.</li>
+        <li>Ellen & Richard Glaisner</li>
+        <li>Ann M. Goris</li>
         <li>Sally & Chip Kubly</li>
-        <li>Beth & Will Kubly</li>
-        <li>Anne Luber & Family</li>
-        <li>Marquette University College of Health Sciences</li>
-        <li>Milwaukee Admirals/ANON Charitable Trust</li>
-        <li>Betsie & Patrick O'Brien</li>
-        <li>Catherine & Bill Perez</li>
-        <li>Power Test</li>
-        <li>The Peggy & Ned Purtell Family</li>
-        <li>Kelly Sherwood Family Fund</li>
+        <li>Bill & Catherine Perez</li>
+        <li>Waukesha County Community Foundation/Laura M. Gralton Charitable Fund</li>
         <li>Angela & Ross Williams</li>
       </ul>
 
@@ -123,97 +109,44 @@ include "header.php";
     <div class="one-third">
       <h3>Navy Level - $1,000</h3>
       <ul>
-        <li>Kerry & John Bolger</li>
-        <li>The Bourne Family</li>
+        <li>Anonymous (2)</li>
+        <li>Elizabeth & Andrew Bruce</li>
         <li>Kathleen & Jim Caragher</li>
-        <li>Jose Carlino Design</li>
-        <li>Dr. William Cullinan</li>
-        <li>Thayre Faust & Friends in memory of Sam Vertelka</li>
-        <li>Anne & Dean Fitzgerald</li>
-        <li>Katie Frommelt & Friends in memory of Garrett Kelley</li>
-        <li>Pamela & Tom Goris</li>
+        <li>Patty & Mike Grebe</li>
         <li>Hammes Family Foundation</li>
-        <li>Kathy & Al Hentzen</li>
-        <li>Susie & Lee Jennings</li>
-        <li>Lily Kubly & Friends</li>
-        <li>Susan Kasten</li>
-        <li>Garry Kelley in memory of Garrett Kelley</li>
-        <li>Kopmeier Family Fund</li>
-        <li>Laura & Pat Koppa</li>
-        <li>Mary Ann & Charles La Bahn</li>
-        <li>Mary Lillydahl</li>
-        <li>Curt & Linda Lundeen</li>
+        <li>Susan & Lee Jennings</li>
+        <li>Mary S. Lillydahl</li>
         <li>Katie & Mark Mohr</li>
-        <li>Candy & Bruce Pindyck</li>
         <li>Jennifer & Mitchell Prosk</li>
-        <li>Pamela & Robert Reily</li>
-        <li>Mary & John Rickmeier</li>
+        <li>Peggy & Bob Schuemann</li>
         <li>Nancy & Greg Smith</li>
-        <li>Julie & Steve Spadafora</li>
+        <li>Julianne & Stephen Spadafora</li>
         <li>Kathleen & Frank Thometz</li>
-        <li>Mary & Chris Tornehl</li>
         <li>Debbie & Camp Van Dyke</li>
-        <li>Brooke & Robert Walker</li>
+        <li>Ellen & Bob Venable</li>
         <li>Mary & Willard Walker</li>
-        <li>Carol K. Wythes</li>
-        <li>Diane & Ed Zore</li>
       </ul>
     </div>
 
     <div class="one-third">
       <h3>Denim Level - $500</h3>
       <ul>
-        <li>Ace Worldwide Elite Relocation Services/Steinergroup</li>
-        <li>Linda & Jack Anick</li>
-        <li>Amy Argall & Family</li>
+        <li>Amy Argall</li>
+        <li>Aversa for Her -- Bayshore</li>
         <li>Bank Five Nine</li>
-        <li>Joan Bell</li>
-        <li>Sally & Peter Blommer</li>
-        <li>Mary & Jeff Brown</li>
-        <li>Elizabeth and Andrew Bruce</li>
-        <li>Burke Candy & Ingredients, Inc.</li>
-        <li>Ginny Bolger</li>
-        <li>Deanna B. Braeger</li>
-        <li>Gretchen & William Burns</li>
-        <li>Kristine H. Cleary & Peter L. Coffey</li>
-        <li>Senator Alberta Darling</li>
-        <li>Ellen & Richard Glaisner</li>
-        <li>Mrs. Marilyn Haker</li>
-        <li>Sharon & Don Hansen</li>
-        <li>Catherine & Phillip Heston</li>
+        <li>Maripat & Tom Dalum</li>
+        <li>Molly & Charlie Dixon</li>
         <li>Diane & Bob Jenkins</li>
-        <li>Jayne Jordan</li>
-        <li>Evelyn Kelly</li>
-        <li>Julie & John Kennedy</li>
-        <li>Lori Kilian in memory of Debbie Thom</li>
-        <li>Rosa Kim</li>
-        <li>Michael Klinker</li>
-        <li>Autumn & David Kwasny</li>
-        <li>The Lanigan Family</li>
+        <li>Jayne J. Jordan</li>
+        <li>Maryglen & Rob Kieckhefer</li>
+        <li>Marlene & John Kreilkamp</li>
         <li>LFF Foundation</li>
-        <li>Margaret & Will Lidstone</li>
-        <li>Peter L. Mahler</li>
-        <li>Debbie & John McGregor</li>
-        <li>Abby Millman & Friends in memory of Debbie Thom</li>
-        <li>Milwaukee Institute of Art & Design</li>
-        <li>Barbara & Henrik Moe</li>
-        <li>Newport Healthcare</li>
-        <li>Linda & Jeffrey Perrigo</li>
-        <li>Kristin Rick & Friends</li>
-        <li>Kris & Robert Rick</li>
-        <li>Lisa & Robbie Robbins</li>
+        <li>Linda & John Mellowes</li>
+        <li>Kristin & Kerry Rick</li>
         <li>Julie & Mason Ross</li>
-        <li>Chloe Salvino & Friends in memory of Debbie Thom</li>
-        <li>Jen & Todd Schoon</li>
-        <li>Patty & Peter Schuyler</li>
-        <li>Stalle Realty Group</li>
-        <li>Joe Ann Stein</li>
-        <li>Barbie Sullivan</li>
-        <li>Betsy Trimble & Family</li>
-        <li>Ellen & Bob Venable</li>
-        <li>Barb & Bob Whealon</li>
-        <li>Abby & Scott Williams</li>
-        <li>Sunny & Sandy Williams</li>
+        <li>Jennifer & Joseph Teglia</li>
+        <li>Betsy Trimble</li>
+        <li>Barbara & Bob Whealon</li>
         <li>Amy & Devin Wolf</li>
       </ul>
 
