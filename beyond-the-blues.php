9989e93f9e5db558cf9644b9b80ad04d2ff211fa
Mark Lippert <mark@foresitegrp.com>
2023-09-05 09:07:30 -0500

    Update sponsors

--- beyond-the-blues.php
+++ beyond-the-blues.php
@@ -83,29 +83,29 @@ include "header.php";
 
       <h3>Teal Level - $5,000</h3>
       <ul>
-        <li>Continental Properties Company, Inc.</li>
-        <li>Charles T. & Jean F. Gorham Fund of the Oshkosh Area Community Foundation</li>
-        <li>The Hauske Family Foundation</li>
-        <li>Cathy & Paul Luber</li>
-        <li>Rogers Behavioral Health</li>
-        <li>Kate & John Wasmer</li>
-        <li>The Sarah & Michael Weiss Family</li>
+<li>Continental Properties Company, Inc.</li>
+<li>Charles T. & Jean F. Gorham Fund of the Oshkosh Area Community Foundation</li>
+<li>The Hauske Family Foundation</li>
+<li>Cathy & Paul Luber</li>
+<li>Rogers Behavioral Health</li>
+<li>Kate & John Wasmer</li>
+<li>The Sarah & Michael Weiss Family</li>
       </ul>
 
       <h3>Turquoise Level - $2,500</h3>
       <ul>
-        <li>Milwaukee Admirals/Anon Charitable Trust</li>
-        <li>Lisa & Souheil Badran</li>
-        <li>The Bourne Family</li>
-        <li>Crescent Grove Advisors</li>
-        <li>Nancy & John Dickens/Godfrey & Kahn, S.C.</li>
-        <li>Executive Benefits Network -- David & Molly Fritz</li>
-        <li>Ellen & Richard Glaisner</li>
-        <li>Ann M. Goris</li>
-        <li>Sally & Chip Kubly</li>
-        <li>Bill & Catherine Perez</li>
-        <li>Waukesha County Community Foundation/Laura M. Gralton Charitable Fund</li>
-        <li>Angela & Ross Williams in memory of Charlie Kubly</li>
+<li>Milwaukee Admirals/Anon Charitable Trust</li>
+<li>Lisa & Souheil Badran</li>
+<li>The Bourne Family</li>
+<li>Crescent Grove Advisors</li>
+<li>Nancy & John Dickens/Godfrey & Kahn, S.C.</li>
+<li>Executive Benefits Network -- David & Molly Fritz</li>
+<li>Ellen & Richard Glaisner</li>
+<li>Ann M. Goris</li>
+<li>Sally & Chip Kubly</li>
+<li>Bill & Catherine Perez</li>
+<li>Waukesha County Community Foundation/Laura M. Gralton Charitable Fund</li>
+<li>Angela & Ross Williams in memory of Charlie Kubly</li>
       </ul>
 
       <!-- <br>
@@ -116,60 +116,71 @@ include "header.php";
     <div class="one-third">
       <h3>Navy Level - $1,000</h3>
       <ul>
-        <li>Anonymous (2)</li>
-        <li>Elizabeth & Andrew Bruce</li>
-        <li>Kathleen & Jim Caragher</li>
-        <li>Jose Carlino Design</li>
-        <li>Anna & George Gaspar</li>
-        <li>Patty & Mike Grebe</li>
-        <li>Hammes Family Foundation</li>
-        <li>Susan & Lee Jennings</li>
-        <li>Stacey & John Koss</li>
-        <li>Mary Ann & Charles LaBahn</li>
-        <li>Mary S. Lillydahl</li>
-        <li>Katie & Mark Mohr</li>
-        <li>Jennifer & Mitchell Prosk</li>
-        <li>Peggy & Ned Purtell</li>
-        <li>Peggy & Bob Schuemann</li>
-        <li>Nancy & Greg Smith</li>
-        <li>Julianne & Stephen Spadafora</li>
-        <li>Kathleen & Frank Thometz</li>
-        <li>Debbie & Camp Van Dyke</li>
-        <li>Ellen & Bob Venable</li>
-        <li>Brooke & Robert Walker</li>
-        <li>Mary & Willard Walker</li>
+<li>Anonymous (2)</li>
+<li>Gwen & Dan Armbrust</li>
+<li>Suzy & John Brennan</li>
+<li>Elizabeth & Andrew Bruce</li>
+<li>Kathleen & Jim Caragher</li>
+<li>Jose Carlino Design</li>
+<li>Anna & George Gaspar</li>
+<li>Patty & Mike Grebe</li>
+<li>Hammes Family Foundation</li>
+<li>Susan & Lee Jennings</li>
+<li>Stacey & John Koss</li>
+<li>Mary Ann & Charles LaBahn</li>
+<li>Mary S. Lillydahl</li>
+<li>Katie & Mark Mohr</li>
+<li>Jennifer & Mitchell Prosk</li>
+<li>Peggy & Ned Purtell</li>
+<li>Peggy & Bob Schuemann</li>
+<li>Nancy & Greg Smith</li>
+<li>Julianne & Stephen Spadafora</li>
+<li>Jennifer & Joseph Teglia</li>
+<li>Kathleen & Frank Thometz</li>
+<li>Debbie & Camp Van Dyke</li>
+<li>Ellen & Bob Venable</li>
+<li>Brooke & Robert Walker</li>
+<li>Mary & Willard Walker</li>
       </ul>
     </div>
 
     <div class="one-third">
       <h3>Denim Level - $500</h3>
       <ul>
-        <li>Ace Enterprises</li>
-        <li>Amy Argall</li>
-        <li>Aversa for Her -- Bayshore</li>
-        <li>Bank Five Nine</li>
-        <li>Deanna Braeger</li>
-        <li>Maripat & Tom Dalum</li>
-        <li>Molly & Charlie Dixon</li>
-        <li>The Grebe Family</li>
-        <li>Marilyn Haker</li>
-        <li>Diane & Bob Jenkins</li>
-        <li>Jayne J. Jordan</li>
-        <li>Maryglen & Rob Kieckhefer</li>
-        <li>Marlene & John Kreilkamp</li>
-        <li>LFF Foundation</li>
-        <li>Linda & John Mellowes</li>
-        <li>Kristin & Kerry Rick</li>
-        <li>Kris & Robert Rick</li>
-        <li>Julie & Mason Ross</li>
-        <li>Jen & Todd Schoon</li>
-        <li>Barbie Sullivan</li>
-        <li>Jennifer & Joseph Teglia</li>
-        <li>Bonnie & Ned Timarac</li>
-        <li>Betsy Trimble</li>
-        <li>Barbara & Bob Whealon</li>
-        <li>Abby & Scott Williams</li>
-        <li>Amy & Devin Wolf</li>
+<li>Ace Enterprises</li>
+<li>Amy Argall</li>
+<li>Aversa for Her -- Bayshore</li>
+<li>Bank Five Nine</li>
+<li>Ginny Bolger</li>
+<li>Deanna Braeger</li>
+<li>Maripat & Tom Dalum</li>
+<li>Sue & Russ Darrow</li>
+<li>Molly & Charlie Dixon</li>
+<li>Liz & Rick Gebhardt</li>
+<li>The Grebe Family</li>
+<li>Marilyn Haker</li>
+<li>Diane & Bob Jenkins</li>
+<li>Jayne J. Jordan</li>
+<li>Susan Kasten</li>
+<li>Evelyn Kelly</li>
+<li>Maryglen & Rob Kieckhefer</li>
+<li>Marlene & John Kreilkamp</li>
+<li>Barbie & Ray Krueger</li>
+<li>LFF Foundation</li>
+<li>Laurie & P. Michael Mahoney</li>
+<li>Debbie & John McGregor</li>
+<li>Linda & John Mellowes</li>
+<li>Kristin & Kerry Rick</li>
+<li>Kris & Robert Rick</li>
+<li>Mary & John Rickmeier</li>
+<li>Julie & Mason Ross</li>
+<li>Jen & Todd Schoon</li>
+<li>Barbie Sullivan</li>
+<li>Bonnie & Ned Timarac</li>
+<li>Betsy Trimble</li>
+<li>Barbara & Bob Whealon</li>
+<li>Abby & Scott Williams</li>
+<li>Amy & Devin Wolf</li>
       </ul>
 
 <!--       <div class="btb-sponsorship">
