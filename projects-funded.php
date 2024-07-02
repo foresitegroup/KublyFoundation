a9d90974e86a39190ee57c27dc42cff008b4db14
Mark Lippert <mark@foresitegrp.com>
2024-07-02 12:04:40 -0500

    Project Funded now gets all info from the database

--- projects-funded.php
+++ projects-funded.php
@@ -4,6 +4,7 @@ $Keywords = "";
 $Description = "";
 
 include "header.php";
+include_once "inc/dbconfig.php";
 ?>
 
 <div class="projects-banner">
@@ -45,149 +46,33 @@ include "header.php";
 
 <div class="funding-current">
   <div class="site-width">
-    <div>
-      <h3><span class="bluetext">01.</span> All 4 Kidz</h3>
-      <div class="content">
-        A grant for the Emotional Healing program supporting kids ages 10-21 to improve their understanding of mental health and wellness through workshops and services.<br>
-        <a href="https://www.all4kidz.org">www.all4kidz.org</a>
-      </div>
-    </div>
+    <?php
+    $currents_stmt = $mysqli->prepare("SELECT * FROM projects_funded WHERE current != '' ORDER BY CASE WHEN title REGEXP '^(A|An|The)[[:space:]]' = 1 THEN TRIM(SUBSTR(title , INSTR(title ,' '))) ELSE title END ASC");
+    $currents_stmt->execute();
+    $currents = $currents_stmt->get_result();
 
-    <div>
-      <h3><span class="bluetext">02.</span> Battlescar Kids Foundation</h3>
-      <div class="content">
-        Funding for Battlescar Kids at Discovery World in Milwaukee; an interactive experience for Milwaukee youth to learn about their inner strength and resiliency.<br>
-        <a href="https://www.battlescarkids.com">www.battlescarkids.com</a>
-      </div>
-    </div>
+    if ($currents->num_rows > 0) {
+      $count = 1;
 
-    <div>
-      <h3><span class="bluetext">03.</span> Boys & Girls Clubs of Greater Milwaukee</h3>
-      <div class="content">
-        A grant to support implementation of SMART Moves curriculum at one club site directed at social-emotional development (SED).<br>
-        <a href="https://www.bgcmilwaukee.org">www.bgcmilwaukee.org</a>
-      </div>
-    </div>
+      foreach ($currents as $current) {
+        echo "<div>\n";
+          echo '<h3><span class="bluetext">'.str_pad($count, 2, '0', STR_PAD_LEFT).".</span> ".$current['title']."</h3>\n";
+          echo '<div class="content">'."\n";
+            echo nl2br($current['description']);
+            if ($current['link'] != "") {
+              $http = (substr($current['link'], 0, 4) === "http") ? "" : "http://";
+              echo "<br>\n".'<a href="'.$http.$current['link'].'">'.$current['link']."</a>\n";
+            }
+          echo "</div>\n";
+        echo "</div>\n";
 
-    <div>
-      <h3><span class="bluetext">04.</span> Compassionate Home Healthcare</h3>
-      <div class="content">
-        Support for a mobile health and hygiene hub bringing needed supplies, educational resources and community engagement activities to those in need.<br>
-        <a href="https://chhcinc.org">chhcinc.org</a>
-      </div>
-    </div>
+        $count++;
+      }
+    }
 
-    <div>
-      <h3><span class="bluetext">05.</span> Eastbrook Academy</h3>
-      <div class="content">
-        Support for continued enhancements to Easbrook's "Warrior Resilience" programming which includes holistic post-pandemic initiatives for K-12 mental health and suicide prevention.<br>
-        <a href="https://eastbrookacademy.org">eastbrookacademy.org</a>
-      </div>
-    </div>
-
-    <div>
-      <h3><span class="bluetext">06.</span> The Friendship Circle</h3>
-      <div class="content">
-        Funding for the "Wish You Knew" podcast featuring teens sharing their mental illness experiences, support and the need for advocacy.<br>
-        <a href="https://www.fcwi.org">www.fcwi.org</a>
-      </div>
-    </div>
-
-    <div>
-      <h3><span class="bluetext">07.</span> Grand Avenue Club</h3>
-      <div class="content">
-        Support of "Secret to Spotlight" effort to increase the number of members of the GAC which provides mental health advocacy in Milwaukee.<br>
-        <a href="https://grandavenueclub.org">grandavenueclub.org</a>
-      </div>
-    </div>
-
-    <div>
-      <h3><span class="bluetext">08.</span> Hometown Heroes</h3>
-      <div class="content">
-        Funding for mental wellness services for children at Camp Hometown Heroes and Camp Reunite.<br>
-        <a href="https://www.camphometownheroes.org">www.camphometownheroes.org</a>
-      </div>
-    </div>
-
-    <div>
-      <h3><span class="bluetext">09.</span> Journey Mental Health Center, Inc.</h3>
-      <div class="content">
-        A grant to support JMHC 24 hour crisis line which provides rapid crisis intervention, individual problem solving and suicide risk assessment.<br>
-        <a href="https://www.journeymhc.org">www.journeymhc.org</a>
-      </div>
-    </div>
-
-    <div>
-      <h3><span class="bluetext">10.</span> Lutheran Social Services of Wisconsin & Upper Michigan</h3>
-      <div class="content">
-        Support for School Centered Mental Health (SCMH) services to serve Milwaukee area children of color impacted by healthcare disparities and inequities.<br>
-        <a href="https://www.lsswis.org">www.lsswis.org</a>
-      </div>
-    </div>
-
-    <div>
-      <h3><span class="bluetext">11.</span> NorthLakes Community Clinic</h3>
-      <div class="content">
-        Funding for the community event "Join or Die" -- a film screening and supporting fair.<br>
-        <a href="https://nlccwi.org">nlccwi.org</a>
-      </div>
-    </div>
-
-    <div>
-      <h3><span class="bluetext">12.</span> Professional Fire Fighters of WI Charitable Foundation</h3>
-      <div class="content">
-        A grant for their fire fighter's support program which prioritizes fire fighter's mental health and provides critical support for them and their families.<br>
-        <a href="https://www.pffw.org">www.pffw.org</a>
-      </div>
-    </div>
-
-    <div>
-      <h3><span class="bluetext">13.</span> Radio Milwaukee</h3>
-      <div class="content">
-        Support for Radio Milwaukee to utilize their airwaves and community programs to normalize conversations around mental health, especially in the artistic community.<br>
-        <a href="https://radiomilwaukee.org">radiomilwaukee.org</a>
-      </div>
-    </div>
-
-    <div>
-      <h3><span class="bluetext">14.</span> REDgen</h3>
-      <div class="content">
-        A grant to enhance and update REDgen's web-based peer-to-peer resiliency site.<br>
-        <a href="https://www.redgen.org">www.redgen.org</a>
-      </div>
-    </div>
-
-    <div>
-      <h3><span class="bluetext">15.</span> Riveredge Nature Center</h3>
-      <div class="content">
-        Support for a mental health initiative including workshops to train community members in QPR suicide prevention program.<br>
-        <a href="https://riveredgenaturecenter.org">riveredgenaturecenter.org</a>
-      </div>
-    </div>
-
-    <div>
-      <h3><span class="bluetext">16.</span> Rocketship Schools Wisconsin</h3>
-      <div class="content">
-        Funding for student and family mental health services expansion including trauma informed care.<br>
-        <a href="https://www.rocketshipschools.org">www.rocketshipschools.org</a>
-      </div>
-    </div>
-
-    <div>
-      <h3><span class="bluetext">17.</span> Safe Families for Children Wisconsin</h3>
-      <div class="content">
-        A project to increase mental health outcomes for parents needing inpatient or outpatient services by increasing the number of volunteers to help care for their families while they are away.<br>
-        <a href="https://safefamilieswi.org">safefamilieswi.org</a>
-      </div>
-    </div>
-
-    <div>
-      <h3><span class="bluetext">18.</span> Wisconsin Collaborative for Healthcare Quality</h3>
-      <div class="content">
-        A grant for "Combatting Stigma: Spreading Awareness Through Visual Narratives"; videos with individuals sharing their lived experience with mental illness to breakdown stigma and encourage selfcare and treatment.<br>
-        <a href="https://www.wchq.org">www.wchq.org</a>
-      </div>
-    </div>
+    $currents->close();
+    $currents_stmt->close();
+    ?>
   </div>
 </div>
 
@@ -211,49 +96,47 @@ include "header.php";
       <div class="allcontrols">
         <div class="controls">
           <div><input type="checkbox" name="control" value="" id="r-all" checked> <label for="r-all">SHOW ALL</label></div>
-          <div><input type="checkbox" name="control" value=".youth" id="r-youth"> <label for="r-youth">YOUTH</label></div>
-          <div><input type="checkbox" name="control" value=".teen" id="r-teen"> <label for="r-teen">TEEN</label></div>
-          <div><input type="checkbox" name="control" value=".college" id="r-college"> <label for="r-college">COLLEGE</label></div>
-          <div><input type="checkbox" name="control" value=".school" id="r-school"> <label for="r-school">SCHOOL</label></div>
-          <div><input type="checkbox" name="control" value=".parents" id="r-parents"> <label for="r-parents">PARENTS</label></div>
-          <div><input type="checkbox" name="control" value=".adult" id="r-adult"> <label for="r-adult">ADULT</label></div>
-          <div><input type="checkbox" name="control" value=".veterans" id="r-veterans"> <label for="r-veterans">VETERANS</label></div>
-          <div><input type="checkbox" name="control" value=".seniors" id="r-seniors"> <label for="r-seniors">SENIORS</label></div>
-          <div><input type="checkbox" name="control" value=".minorities" id="r-minorities"> <label for="r-minorities">BIPOC</label></div>
-          <div><input type="checkbox" name="control" value=".lgbtq" id="r-lgbtq"> <label for="r-lgbtq">LGBTQ+</label></div>
-          <div><input type="checkbox" name="control" value=".men" id="r-men"> <label for="r-men">MEN</label></div>
-          <div><input type="checkbox" name="control" value=".women" id="r-women"> <label for="r-women">WOMEN</label></div>
-          <div><input type="checkbox" name="control" value=".suicide" id="r-suicide"> <label for="r-suicide">SUICIDE</label></div>
-          <div><input type="checkbox" name="control" value=".mentalhealth" id="r-mentalhealth"> <label for="r-mentalhealth">MENTAL HEALTH</label></div>
-          <div><input type="checkbox" name="control" value=".training" id="r-training"> <label for="r-training">TRAINING</label></div>
-          <div><input type="checkbox" name="control" value=".awareness" id="r-awareness"> <label for="r-awareness">AWARENESS</label></div>
-          <div><input type="checkbox" name="control" value=".professionaltraining" id="r-professionaltraining"> <label for="r-professionaltraining">PROFESSIONAL TRAINING</label></div>
-          <div><input type="checkbox" name="control" value=".awarenesssupport" id="r-awarenesssupport"> <label for="r-awarenesssupport">AWARENESS SUPPORT</label></div>
-          <div><input type="checkbox" name="control" value=".spiritualmindfulness" id="r-spiritualmindfulness"> <label for="r-spiritualmindfulness">SPIRITUAL / MINDFULNESS</label></div>
+
+          <?php
+          $pf_cats_stmt = $mysqli->prepare("SELECT * FROM projects_funded_categories ORDER BY category ASC");
+          $pf_cats_stmt->execute();
+          $pf_cats = $pf_cats_stmt->get_result();
+
+          if ($pf_cats->num_rows > 0) {
+            foreach ($pf_cats as $pf_cat) {
+              echo '<div><input type="checkbox" name="control" value=".'.strtolower(preg_replace("/[^A-Za-z0-9]/", '', $pf_cat['category'])).'" id="r-'.strtolower(preg_replace("/[^A-Za-z0-9]/", '', $pf_cat['category'])).'"> <label for="r-'.strtolower(preg_replace("/[^A-Za-z0-9]/", '', $pf_cat['category'])).'">'.strtoupper($pf_cat['category'])."</label></div>\n";
+            }
+          }
+
+          $pf_cats->close();
+          $pf_cats_stmt->close();
+          ?>
         </div>
       </div>
     </div>
 
     <div id="mixcontainer">
       <?php
-      include_once "inc/dbconfig.php";
+      $projects_stmt = $mysqli->prepare("SELECT * FROM projects_funded ORDER BY CASE WHEN title REGEXP '^(A|An|The)[[:space:]]' = 1 THEN TRIM(SUBSTR(title , INSTR(title ,' '))) ELSE title END ASC, year DESC");
+      $projects_stmt->execute();
+      $projects = $projects_stmt->get_result();
 
-      $result = $mysqli->query("SELECT * FROM projects_funded ORDER BY
-        CASE
-          WHEN title REGEXP '^(A|An|The)[[:space:]]' = 1 THEN
-            TRIM(SUBSTR(title , INSTR(title ,' ')))
-          ELSE title
-        END
-        ASC, year DESC");
-
-      while($row = $result->fetch_array(MYSQLI_ASSOC)) {
-        echo "<div class=\"mix " . $row['categories'] . "\">\n";
-        echo "<h4>" . $row['title'] . "</h4>\n";
-        echo nl2br($row['description']);
-        echo " (" . $row['year'] . ")<br>\n";
-        if ($row['link'] != "") echo "<a href=\"http://" . $row['link'] . "\">" . $row['link'] . "</a>\n";
-        echo "</div>\n\n";
+      if ($projects->num_rows > 0) {
+        foreach ($projects as $project) {
+          echo '<div class="mix '.$project['categories'].'">'."\n";
+          echo "<h4>".$project['title']."</h4>\n";
+          echo nl2br($project['description']);
+          if ($project['year'] != "") echo " (".$project['year'].")<br>\n";
+          if ($project['link'] != "") {
+            $http = (substr($project['link'], 0, 4) === "http") ? "" : "http://";
+            echo '<a href="'.$http.$project['link'].'">'.$project['link']."</a>\n";
+          }
+          echo "</div>\n\n";
+        }
       }
+
+      $projects->close();
+      $projects_stmt->close();
       ?>
     </div>
   </div>
