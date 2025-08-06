0fdb726c4e52182d4c8cb8d302d759a3622b2c90
Mark Lippert <mark@foresitegrp.com>
2025-08-06 12:03:15 -0500

    Update newsletter

--- news/wp-content/themes/kublyfoundation/footer.php
+++ news/wp-content/themes/kublyfoundation/footer.php
@@ -16,22 +16,24 @@ if (is_home()) {
 
 <div id="newsletters">
   <div class="site-width">
-    <a href="<?php echo $TopDir; ?>pdf/Kubly_Newsletter_Winter_2025.pdf" class="image"><img src="<?php echo $TopDir; ?>images/Kubly_Newsletter_Winter_2025.webp" alt="" width="1000" height="1294"></a>
+    <a href="<?php echo $TopDir; ?>pdf/Kubly_Newsletter_Summer_2025.pdf" class="image"><img src="<?php echo $TopDir; ?>images/Kubly_Newsletter_Summer_2025.webp" alt="" width="1000" height="1294"></a>
 
     <div class="text">
-      <h3>Winter 2025 Newsletter</h3>
+      <h3>Summer 2025 Newsletter</h3>
 
       <span>What's Inside</span><br>
-      <span>01.</span> Rocketship School<br>
-      <span>02.</span> Projects Funded<br>
-      <span>03.</span> Beyond the Blues 2024 Recap<br>
-      <span>05.</span> Annual Financial Report<br>
-      <span>06.</span> C4C and Paddle With A Purpose<br>
+      <span>01.</span> Exciting Changes<br>
+      <span>02.</span> 5th YLC Golf Outing<br>
+      <span>03.</span> Milwaukee Urban Stables<br>
+      <span>03.</span> Legacy Giving Defined<br>
+      <span>04.</span> Projects Funded<br>
+      <span>05.</span> Thank You Donors<br>
       <br>
 
       <h4>Past Newsletters</h4>
       <div id="past-newsletters">
         <div>
+          <a href="<?php echo $TopDir; ?>pdf/Kubly_Newsletter_Summer_2025.pdf">Summer 2025</a><br>
           <a href="<?php echo $TopDir; ?>pdf/Kubly_Newsletter_Summer_2024.pdf">Summer 2024</a><br>
           <a href="<?php echo $TopDir; ?>pdf/Kubly_Newsletter_Summer_2023.pdf">Summer 2023</a><br>
           <a href="<?php echo $TopDir; ?>pdf/Kubly_Newsletter_Summer_2022.pdf">Summer 2022</a><br>
